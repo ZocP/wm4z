35234428105524f18c8753949d215b877326ff36
Kevin <pidersk@gmail.com>
2022-01-16 12:52:53 -0500

    added calendar

---
+++ src/components/Calender.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <antd>
+
+    </antd>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calender",
+  components:{
+    Antd
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Calender.vue
+++ src/views/Calender.vue
@@ -1,19 +1,49 @@
 <template>
   <div class = "calender">
+    <div class = "">
+      <Calendar class = "calender_container ">
 
+      </Calendar>
+    </div>
   </div>
+
+
 </template>
 
 <script>
+
+import Calendar from 'ant-design-vue/lib/calendar';
+import 'ant-design-vue/lib/calendar/style/css';
+
 export default {
+  data:function(){
+    return{
+
+    }
+  },
   name: "Calender",
+  components:{
+    Calendar,
+  }
 };
 </script>
 
 <style scoped>
   .calender{
+    display: flex;
+    justify-content: center;
+    align-items: center;
     height : 100vh;
     width: 100%;
     background-color: #A42121;
+  }
+  .calender_container{
+    box-shadow: 0px 4px 10px 5px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    width: 80vw;
+    background-color: #e6e4d7;
+  }
+  .ant-fullcalendar-header{
+    display: none;
   }
 </style>
